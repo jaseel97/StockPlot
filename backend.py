a3ffc5ae5dd69c14bd965989ab59ef4701142ea3
George V Jose <jorujose@gmail.com>
2017-11-22 23:09:19 +0530

    Complted commit. v1.0

--- backend.py
+++ backend.py
@@ -9,11 +9,11 @@ from credentials import db_username, db_password, db_name
 
 def get_time_series(time):
     time = time.lower()
-    if time=="day":
+    if time=='daily':
         return "TIME_SERIES_DAILY"
-    elif time=="week":
+    elif time=='weekly':
         return "TIME_SERIES_WEEKLY"
-    elif time=="month":
+    elif time=='monthly':
         return "TIME_SERIES_MONTHLY"
     else:
         print("INFO : Wrong Time Series. Resetting to Day")
@@ -27,24 +27,27 @@ def insert_into_db(df, table_name):
     con_str = 'mysql+pymysql://{}:{}@localhost:3306/mysql'.format(db_username, db_password)
     con = create_engine(con_str, echo=False)
     df.to_sql(name=table_name, con=con, if_exists='replace')
-    print("INFO : DataBase Created.")
+    print("INFO : DataBase Created for table {}".format(table_name))
     return con
 
 def get_data_frame(stock_name, time):
     time_series = get_time_series(time)
-    print("INFO : Downloading Data")
+
+    print("INFO : Downloading Data for {}".format(stock_name))
     url_csv = "https://www.alphavantage.co/query?function={}&symbol={}&interval=15min&outputsize=compact&apikey={}&datatype=csv".format(time_series, stock_name, api_key)
     pd_data = pd.read_csv(url_csv)
     print("INFO : Download Complete")
-    create_temp_csv(pd_data, stock_name)
 
+    create_temp_csv(pd_data, stock_name)
     return pd_data
 
     # insert_into_db(pd_data, stock_name)
 
+
 def plt_plot(data, plot_name, num_items):
     x_axis_real = [i[0] for i in data[:num_items]]
     y_axis = [i[1] for i in data[:num_items]]
+
     x_axis = range(len(x_axis_real))
     plt.xticks(x_axis, x_axis_real, rotation='vertical')
 
@@ -54,26 +57,36 @@ def plt_plot(data, plot_name, num_items):
     plt.ylabel('Values')
     plt.show()
 
-def plt_two_plot(data1, data2, plot_name):
-    x_axis_real1 = [i[0] for i in data1[:50]]
-    y_axis1 = [i[1] for i in data1[:50]]
 
-    x_axis_real2 = [i[0] for i in data2[:50]]
-    y_axis2 = [i[1] for i in data2[:50]]
+def plt_two_plot(table_name1, table_name2, data1, data2, plot_name, num_items):
+    x_axis_real1 = [i[0] for i in data1[:num_items]]
+    y_axis1 = [i[1] for i in data1[:num_items]]
+
+    x_axis_real2 = [i[0] for i in data2[:num_items]]
+    y_axis2 = [i[1] for i in data2[:num_items]]
+
     x_axis = range(len(x_axis_real1))
     plt.xticks(x_axis, x_axis_real1, rotation='vertical')
     plt.xticks(x_axis, x_axis_real2, rotation='vertical')
 
-    plt.plot(x_axis, y_axis1)
-    plt.plot(x_axis, y_axis2)
+    plt.plot(x_axis, y_axis1, label=table_name1)
+    plt.plot(x_axis, y_axis2, label=table_name2)
     plt.title(plot_name)
     plt.xlabel('Time Stamp')
     plt.ylabel('Values')
+    plt.legend()
     plt.show()
 
-def plot_two_data(table_name1, table_name2, con):
+
+def plot_two_data(table_name1, table_name2, con, num_items):
     df1 = pd.read_sql_table(table_name1, con)
     df2 = pd.read_sql_table(table_name2, con)
+    df1 = df1[['timestamp', 'close']]
+    df2 = df2[['timestamp', 'close']]
+    data1 = df1.values
+    data2 = df2.values
+    plot_name = table_name1+" vs "+table_name2
+    plt_two_plot(table_name1, table_name2, data1, data2, plot_name, num_items)
 
 #PLOT DATA
 def plot_data(table_name, con, num_items):
@@ -92,4 +105,3 @@ def load_data(stock_name, time):
 
 # con = load_data("GOOGL", "day")
 # plot_data("GOOGL", con)
-# plt_two_plot([[1,2], [3, 4]], [[3,4], [5,6]], "name")
--- ui.py
+++ ui.py
@@ -1,17 +1,16 @@
 import tkinter as tk
 from tkinter.ttk import *
 import tkinter.messagebox as tkm
-from PIL import Image, ImageTk
 
-from backend import load_data, plot_data
+from backend import load_data, plot_data, plot_two_data
 
-base = tk.Tk()
+base = tk.Tk()					# bottommost/base/root window definition
 base.title("StockPlot");
-base.geometry("600x700")
+base.geometry("600x800")
 
-title_text = 'StockPlot';
+title_text = 'StockPlot';       # heading inside window
 title_label = tk.Label(base,text=title_text,font=("Helvetica",20,"bold"))
-title_label.pack(pady=30)
+title_label.pack(pady=15)
 
 header1_text="Collect Stock Data"
 header1_label = tk.Label(base,text = header1_text,font=("Helvetica,15,bold"))
@@ -22,10 +21,10 @@ addstock_default = 'Enter Stock Name'
 addstock_entry.insert(0,addstock_default)
 addstock_entry.pack()
 
-stocks = ['GOOG','YHOO'," "] # replace with all stocks preent in db + append a  " " at the end
+stocks = ['GOOGL','TSLA'," "] # replace with all stocks present in db + append a  " " at the end
 stocks.sort()
 
-def addStock():
+def addStock():         # when add button is pressed, this fuction is executed
 	stockname = addstock_entry.get()
 	if stockname==addstock_default:
 		tkm.showinfo("Info","Please insert a stock name!")
@@ -33,20 +32,20 @@ def addStock():
 		if stockname in stocks:
 			tkm.showinfo("Info","Stock "+stockname+" already present in Database!")
 		else:
-			tkm.showinfo("Info","Added Successfully!")
 			fetchStockData(stockname)
+			tkm.showinfo("Info","Added Successfully!")
 
-def fetchStockData(stockname):
-	print("fetching")
+def fetchStockData(stockname):    # put fetching stock data and storing in db here
+	# print("fetching")              # replace print statement with you know what B-)
 	updateList(stockname)
 	addstock_entry.delete(0,'end')
 	addstock_entry.insert(0,addstock_default)
 
 def updateList(stockname):
 	stocks.append(stockname)
-	print(stocks)
-	stock_dropdown.set_menu(stocks[0],*stocks)
-	stock_dropdown2.set_menu(stocks[0],*stocks)
+	# print(stocks)                  # you dont really need this. just checking if it actually works
+	stock_dropdown.set_menu(stocks[0],*stocks)      #update dropdown list
+	stock_dropdown2.set_menu(stocks[0],*stocks)     #update dropdown list
 
 
 addstock_button = tk.Button(base,text="Add",command=addStock)
@@ -55,11 +54,11 @@ addstock_button.pack()
 
 header2_text="Pick Stock"
 header2_label = tk.Label(base,text = header2_text,font=("Helvetica,15,bold"))
-header2_label.pack(pady=(20,2))
+header2_label.pack(pady=(15,2))
 
 stock1 = tk.StringVar(base)
 stock1.set(stocks[0])
-stock_dropdown = OptionMenu(base,stock1,*stocks) ####
+stock_dropdown = OptionMenu(base,stock1,*stocks)
 stock_dropdown.config(width=14)
 stock_dropdown.pack()
 
@@ -69,27 +68,60 @@ stock_dropdown2 = OptionMenu(base,stock2,*stocks)
 stock_dropdown2.config(width=14)
 stock_dropdown2.pack(pady=(5,1))
 
-def plotPress():
-	s1 = stock1.get()
-	s2 = stock2.get()
-	if s1==' ' and s2==' ':
-		tkm.showinfo("Info","Select atleast one stock to plot!")
-	elif s2==' ':
-		print("Plot only stock1")
-	elif s1==' ':
-		print("Plot only stock2")
-	elif s1==s2:
-		print("PLot only stock1")
-	else:
-		print("Plot stock1 and stock2")
+header3_text="Pick Timescale"
+header3_label = tk.Label(base,text = header3_text,font=("Helvetica,15,bold"))
+header3_label.pack(pady=(15,2))
+
+timescale1 = tk.StringVar(base)
+timescale = [' ','Daily','Weekly','Monthly']
+timescale1.set(timescale[0])
+timescale_dropdown = OptionMenu(base,timescale1,*timescale)
+timescale_dropdown.config(width=14)
+timescale_dropdown.pack()
+
+header4_text="Pick Datapoint Count"
+header4_label = tk.Label(base,text = header4_text,font=("Helvetica,15,bold"))
+header4_label.pack(pady=(15,2))
+
+datapoint1 = tk.StringVar(base)
+datapoint = [' ','5','10','15','20','25','30','50','75','100']   # type cast to str to get datapoint value. values given as strings to avoid problems while making dropdown
+datapoint1.set(datapoint[0])
+datapoint_dropdown = OptionMenu(base,datapoint1,*datapoint)
+datapoint_dropdown.config(width=14)
+datapoint_dropdown.pack()
+
+def onPlotPress():
+    s1 = stock1.get() #stock1 name as str
+    s2 = stock2.get() #stock2 name as str
+    t1 = timescale1.get() # timescale 'Daily' 'Weekly' or 'Monthly'
+    d1 = datapoint1.get() # datapoint count as str
+    if t1 == ' ':
+        tkm.showinfo('Info','Select a Timescale!')
+    if d1 == ' ':
+        tkm.showinfo('Info','Select number of data points!')
+    if s1==' ' and s2==' ':
+        tkm.showinfo("Info","Select at least one stock to plot!")
+    elif s2==' ':   #Stock 1
+        con = load_data(s1, t1)
+        plot_data(s1, con, int(d1))
+    elif s1==' ':
+        con = load_data(s2, t1)
+        plot_data(s1, con, int(d1))
+    elif s1==s2:
+        con = load_data(s1, t1)
+        plot_data(s1, con, int(d1))
+    else:
+        con = load_data(s1, t1)
+        load_data(s2, t1)
+        plot_two_data(s1, s2, con, int(d1))
+        # print("Plot stock1 and stock2")            # add if possible else leave it  :-P
 
 
-plot_button = tk.Button(base,text="Plot",command=plotPress)
+plot_button = tk.Button(base,text="Plot",command=onPlotPress)
 plot_button.config(width=17)
-plot_button.pack(pady=(20,1))
+plot_button.pack(pady=(15,1))
 
-# bg = tk.PhotoImage(file="bg.png")
-bg = ImageTk.PhotoImage("bg.png")
+bg = tk.PhotoImage(file="bg.png")
 bg_label = tk.Label(base,image=bg)
 bg_label.pack()
 
